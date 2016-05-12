66d74bb370ff67de72cb167ec28338e1493c7b18
Jhen <developer@jhen.me>
2016-05-12 18:42:42 +0800

    Implement AudioPlayer and export playMusic, stopMusic, stopAllMusic methods

---
+++ android/src/main/java/com/mybigday/rn/AudioPlayer.java
@@ -0,0 +1,68 @@
+package com.mybigday.rn;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+
+public class AudioPlayer {
+    private Context context;
+    private ReactApplicationContext reactContext;
+    private Map<String, MediaPlayer> audioIdMap = new HashMap<String, MediaPlayer>();
+
+    public AudioPlayer(Context context, ReactApplicationContext reactContext) {
+        this.context = context;
+        this.reactContext = reactContext;
+    }
+
+    public WritableMap playMusic(String filePath) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, Uri.parse(filePath));
+        mediaPlayer.start();
+
+        String id = UUID.randomUUID().toString();
+        audioIdMap.put(id, mediaPlayer);
+
+        WritableMap evt = Arguments.createMap();
+        evt.putString("musicId", id);
+        sendEvent("MusicStart", evt);
+
+        int duration = mediaPlayer.getDuration();
+        WritableMap result = Arguments.createMap();
+        result.putString("id", id);
+        result.putInt("duration", duration);
+        return result;
+    }
+
+    public void stopMusic(String id) {
+        MediaPlayer mediaPlayer = audioIdMap.get(id);
+        if (mediaPlayer != null) {
+            audioIdMap.get(id).stop();
+            audioIdMap.remove(id);
+        }
+    }
+
+    public void stopAllMusic() {
+        Iterator it = audioIdMap.entrySet().iterator();
+        while (it.hasNext()) {
+            MediaPlayer mediaPlayer = (MediaPlayer) it.next();
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+            }
+            it.remove();
+        }
+    }
+
+    protected void sendEvent(String eventName, WritableMap params) {
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+}
--- android/src/main/java/com/mybigday/rn/RNMediaPlayer.java
+++ android/src/main/java/com/mybigday/rn/RNMediaPlayer.java
@@ -1,18 +1,18 @@
 package com.mybigday.rn;
 
 import android.app.Activity;
-import android.content.Context;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.LifecycleEventListener;
 
 public class RNMediaPlayer extends ReactContextBaseJavaModule {
-  private Context context;
+  private AudioPlayer audioPlayer;
+  private ReactApplicationContext context;
   private Activity activity;
   private ExternalDisplay externalDisplay;
   private boolean alreadyInitialize = false;
@@ -20,7 +20,7 @@ public class RNMediaPlayer extends ReactContextBaseJavaModule {
   public RNMediaPlayer(ReactApplicationContext reactContext, Activity activity) {
     super(reactContext);
 
-    this.context = (Context) reactContext;
+    this.context = reactContext;
     this.activity = activity;
   }
 
@@ -29,10 +29,12 @@ public class RNMediaPlayer extends ReactContextBaseJavaModule {
     return "RNMediaPlayer";
   }
 
+
   @ReactMethod
   public void initialize() {
     if (!alreadyInitialize) {
-      externalDisplay = new ExternalDisplay((Context) this.activity, (ReactApplicationContext) this.context);
+      audioPlayer = new AudioPlayer(activity, context);
+      externalDisplay = new ExternalDisplay(activity, context);
       externalDisplay.start();
 
       alreadyInitialize = true;
@@ -40,9 +42,30 @@ public class RNMediaPlayer extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void showVirtualScreen(boolean bool) {
-    if (externalDisplay == null) return;
+  public void showVirtualScreen(boolean bool, Promise promise) {
+    if (!alreadyInitialize) {
+      promise.resolve(false);
+      return;
+    }
 
     externalDisplay.showVirtualScreen(bool);
+    promise.resolve(true);
   }
+
+  @ReactMethod
+  public void playMusic(String filePath, Promise promise) {
+    WritableMap result = audioPlayer.playMusic(filePath);
+    promise.resolve(result);
+  }
+
+  @ReactMethod
+  public void stopMusic(String id) {
+    audioPlayer.stopMusic(id);
+  }
+
+  @ReactMethod
+  public void stopAllMusic() {
+    audioPlayer.stopAllMusic();
+  }
+
 }
