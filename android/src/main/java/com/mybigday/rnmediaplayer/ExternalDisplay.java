b8bf090b3f80c76ec73da7f1dc3b4b42dff9fb28
Jhen <developer@jhen.me>
2016-05-19 15:43:32 +0800

    [Android] Split screen handle to ExternalDisplayHelper, and fix switch problem

--- android/src/main/java/com/mybigday/rnmediaplayer/ExternalDisplay.java
+++ android/src/main/java/com/mybigday/rnmediaplayer/ExternalDisplay.java
@@ -5,25 +5,24 @@ import android.app.Activity;
 import android.app.Presentation;
 import android.content.Context;
 import android.graphics.Color;
-import android.media.MediaRouter;
-import android.media.MediaRouter.SimpleCallback;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
+import android.hardware.display.DisplayManager;
 
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-class ExternalDisplayPresentation extends Presentation {
+class ExternalDisplayScreen extends Presentation {
   private View containerView;
 
-  ExternalDisplayPresentation(Context ctx, Display display, View containerView) {
+  ExternalDisplayScreen(Context ctx, Display display, View containerView) {
     super(ctx, display);
 
     this.containerView = containerView;
@@ -32,33 +31,119 @@ class ExternalDisplayPresentation extends Presentation {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     setContentView(containerView);
   }
 }
 
-public class ExternalDisplay implements LifecycleEventListener {
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+class ExternalDisplayHelper implements DisplayManager.DisplayListener {
+  public interface Listener {
+    void showScreen(Display display);
+    void clearScreen();
+  }
+
+  private Listener listener = null;
+  private DisplayManager dm = null;
+  private Display currentDisplay = null;
+  private boolean alreadyRouted = true;
+  private boolean enabled = true;
+
+  public ExternalDisplayHelper(Context context, Listener listener) {
+    this.listener = listener;
+
+    dm = (DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE);
+  }
+
+  public void onResume() {
+    handleRoute();
+    dm.registerDisplayListener(this, null);
+  }
+
+  public void onPause() {
+    listener.clearScreen();
+    currentDisplay = null;
+
+    dm.unregisterDisplayListener(this);
+  }
+
+  private void handleRoute() {
+    if (!enabled) return;
+
+    Display[] displays = dm.getDisplays(DisplayManager.DISPLAY_CATEGORY_PRESENTATION);
+
+    if (displays.length == 0) {
+      if (currentDisplay != null || alreadyRouted) {
+        listener.clearScreen();
+        currentDisplay = null;
+      }
+    } else {
+      Display display = displays[0];
+      if (display != null && display.isValid()) {
+        if (currentDisplay == null) {
+          listener.showScreen(display);
+          currentDisplay = display;
+        } else if (currentDisplay.getDisplayId() != display.getDisplayId()) {
+          listener.clearScreen();
+          listener.showScreen(display);
+          currentDisplay = display;
+        }
+      } else if (currentDisplay != null) {
+        listener.clearScreen();
+        currentDisplay = null;
+      }
+    }
+    alreadyRouted = false;
+  }
+
+  @Override
+  public void onDisplayAdded(int displayId) {
+    handleRoute();
+  }
+
+  @Override
+  public void onDisplayChanged(int displayId) {
+    handleRoute();
+  }
+
+  @Override
+  public void onDisplayRemoved(int displayId) {
+    handleRoute();
+  }
+}
+
+public class ExternalDisplay implements LifecycleEventListener, ExternalDisplayHelper.Listener {
   private Root root;
   private Context context;
   private ReactApplicationContext reactContext;
   private LinearLayout containerView;
-  private ExternalDisplayPresentation preso;
+  private ExternalDisplayScreen screen;
   private Preview preview;
   private boolean isShowVirtualScreen = false;
-
-  MediaRouter router = null;
-  SimpleCallback cb = null;
+  private boolean alreadyStarted = false;
+  private ExternalDisplayHelper helper = null;
 
   ExternalDisplay(Context ctx, ReactApplicationContext reactContext) {
     this.context = ctx;
     this.reactContext = reactContext;
 
     reactContext.addLifecycleEventListener(this);
+    helper = new ExternalDisplayHelper(context, this);
+  }
+
+  public void start() {
+    if (alreadyStarted) return;
+    initLayoutView();
+    tryShowPreview();
+    
+    root = new Root(context, reactContext, containerView);
+    alreadyStarted = true;
   }
 
   public void showVirtualScreen(boolean bool) {
+    if (screen != null) return;
+
     isShowVirtualScreen = bool;
-    if (bool) {
+    if (isShowVirtualScreen) {
       tryShowPreview();
     } else {
       tryRemovePreview();
@@ -69,87 +154,6 @@ public class ExternalDisplay implements LifecycleEventListener {
     return root;
   }
 
-  public void start() {
-    initLayoutView();
-    tryShowPreview();
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-      if (cb == null) {
-        cb = new RouteCallback();
-        router = (MediaRouter) context.getSystemService(Context.MEDIA_ROUTER_SERVICE);
-      }
-
-      handleRoute(router.getSelectedRoute(MediaRouter.ROUTE_TYPE_LIVE_VIDEO));
-      router.addCallback(MediaRouter.ROUTE_TYPE_LIVE_VIDEO, cb);
-    }
-    root = new Root(context, reactContext, containerView);
-  }
-
-  @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-  private void handleRoute(MediaRouter.RouteInfo route) {
-    if (route == null) {
-      clearPreso();
-    } else {
-      Display display = route.getPresentationDisplay();
-      if (route.isEnabled() && display != null) {
-        if (preso == null) {
-          showPreso(route);
-        } else if (preso.getDisplay().getDisplayId() != display.getDisplayId()) {
-          clearPreso();
-          showPreso(route);
-        }
-      } else {
-        clearPreso();
-      }
-    }
-  }
-
-  @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-  private void showPreso(MediaRouter.RouteInfo route) {
-    preso = new ExternalDisplayPresentation(this.context, route.getPresentationDisplay(), containerView);
-
-    tryRemovePreview();
-    preso.show();
-  }
-
-  @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-  private void clearPreso() {
-    if (preso == null) return;
-    preso.dismiss();
-    preso = null;
-
-    tryShowPreview();
-  }
-
-  private void handlePause() {
-    if (root != null) {
-      root.destroyContainer();
-    }
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-      clearPreso();
-
-      if (router != null) {
-        router.removeCallback(cb);
-      }
-    }
-    tryRemovePreview();
-  }
-
-  @Override
-  public void onHostResume() {
-    start();
-    showVirtualScreen(true);
-  }
-
-  @Override
-  public void onHostPause() {
-    showVirtualScreen(false);
-    handlePause();
-  }
-
-  @Override
-  public void onHostDestroy() {}
-
-  // test
   private void initLayoutView() {
     containerView = new LinearLayout(this.context);
     LayoutParams params = new LayoutParams(
@@ -158,6 +162,15 @@ public class ExternalDisplay implements LifecycleEventListener {
     );
     containerView.setLayoutParams(params);
     containerView.setBackgroundColor(Color.BLACK);
+  }
+
+  private void initPreview() {
+    tryRemovePreview();
+    DisplayMetrics displaymetrics = new DisplayMetrics();
+    ((Activity) this.context).getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+    int height = displaymetrics.heightPixels;
+    int width = displaymetrics.widthPixels;
+    preview = new Preview(this.context, containerView, (int) (width * 0.3), (int) (height * 0.3));
   }
 
   private void tryShowPreview() {
@@ -172,21 +185,39 @@ public class ExternalDisplay implements LifecycleEventListener {
     preview = null;
   }
 
-  private void initPreview() {
+  @Override
+  public void showScreen(Display display) {
     tryRemovePreview();
-    DisplayMetrics displaymetrics = new DisplayMetrics();
-    ((Activity) this.context).getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-    int height = displaymetrics.heightPixels;
-    int width = displaymetrics.widthPixels;
-    preview = new Preview(this.context, containerView, (int) (width * 0.3), (int) (height * 0.3));
+    screen = new ExternalDisplayScreen(this.context, display, containerView);
+    screen.show();
   }
 
-  @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-  class RouteCallback extends MediaRouter.SimpleCallback {
-    @Override
-    public void onRoutePresentationDisplayChanged(MediaRouter router,
-                            MediaRouter.RouteInfo route) {
-      handleRoute(route);
-    }
+  @Override
+  public void clearScreen() {
+    if (screen == null) return;
+
+    ViewGroup parent = (ViewGroup) containerView.getParent();
+    if (parent != null) parent.removeView(containerView);
+
+    screen.dismiss();
+    screen = null;
+    tryShowPreview();
   }
+
+  @Override
+  public void onHostResume() {
+    start();
+    helper.onResume();
+    showVirtualScreen(true);
+  }
+
+  @Override
+  public void onHostPause() {
+    showVirtualScreen(false);
+    helper.onPause();
+    tryRemovePreview();
+  }
+
+  @Override
+  public void onHostDestroy() {}
 }
