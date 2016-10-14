f2e383b32fd4c7445cecfe89994c5bb55717fcd8
Jhen <developer@jhen.me>
2016-10-14 13:06:53 +0800

    Update example

--- example/android/app/src/main/java/com/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/MainActivity.java
@@ -1,14 +1,6 @@
 package com.example;
 
 import com.facebook.react.ReactActivity;
-import com.rnfs.RNFSPackage;
-import com.mybigday.rnmediaplayer.RNMediaPlayerPackage;
-import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends ReactActivity {
 
@@ -19,28 +11,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "example";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new RNFSPackage(),
-            new RNMediaPlayerPackage(this),
-            new ExtraDimensionsPackage(this)
-        );
     }
 }
