4fecdac9dff1a89233f69694a771ba5e7d022f65
Pablo Soares <pablo@mw8solucoes.com.br>
2017-06-13 18:05:13 -0300

    Prevent NPE on onHostResume
    This call to helper.onResume() was causing intermitent NPE's
    They happened during app initialization but I could not identify the reason; usually after a reload

--- android/src/main/java/com/mybigday/rnmediaplayer/ExternalDisplay.java
+++ android/src/main/java/com/mybigday/rnmediaplayer/ExternalDisplay.java
@@ -142,7 +142,7 @@ public class ExternalDisplay implements LifecycleEventListener, ExternalDisplayH
     if (alreadyStarted) return;
     initLayoutView();
     tryShowPreview();
-    
+
     root = new Root(context, reactContext, containerView);
     alreadyStarted = true;
   }
@@ -244,14 +244,20 @@ public class ExternalDisplay implements LifecycleEventListener, ExternalDisplayH
   @Override
   public void onHostResume() {
     start();
-    helper.onResume();
+    // Don't know why this would be null at this point
+    if (helper != null) {
+      helper.onResume();
+    }
     showVirtualScreen(true);
   }
 
   @Override
   public void onHostPause() {
     showVirtualScreen(false);
-    helper.onPause();
+    // Don't know why this would be null at this point
+    if (helper != null) {
+      helper.onResume();
+    }
     tryRemovePreview();
   }
 
